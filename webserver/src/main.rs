2df7a422c2d15327e9165db0cf4c4e54ec670d0b
Stefan Scheller <ste.scheller@gmail.com>
2023-04-15 23:31:27 +0200

    copied over webserver example

---
+++ webserver/src/main.rs
@@ -0,0 +1,51 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+use webserver::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::build(4).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+
+    let request_line = &http_request[0];
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let length = content.len();
+
+    let response = format!("{status_line}\r\nContent-length: {length}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
