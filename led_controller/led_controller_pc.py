de66018cd4b0f3c4f00dcfaf87174fbf82f78bd5
Stefan Scheller <stefan.scheller@endress.com>
2023-06-13 14:21:00 +0200

    updated led controller simulation
    - added output to shell of request and response
    - updated supported commands

--- led_controller/led_controller_pc.py
+++ led_controller/led_controller_pc.py
@@ -6,7 +6,9 @@ import serial
 
 class LedController:
     COMMANDS = {
-        ("led", "on"): lambda: b"ON\r\n",
+        ("led", "red"): lambda: b"RED\r\n",
+        ("led", "green"): lambda: b"GREEN\r\n",
+        ("led", "blue"): lambda: b"BLUE\r\n",
         ("led", "off"): lambda: b"OFF\r\n",
     }
 
@@ -18,13 +20,16 @@ class LedController:
         self.serial = s
 
     def parse_line(self, line: str) -> None:
+        print(f">>> {line}")
         key = tuple(item.lower().strip() for item in line.split(" "))
         command = LedController.COMMANDS.get(key, lambda: b"invalid command\r\n")
         self.serial.write(command())
+        print(f"<<< {str(command(), 'utf-8')}")
 
     def run(self) -> None:
         try:
             self.serial.open()
+            print(f"Listening on {self.serial.port} ({self.serial.baudrate} Baud) ..")
             while self.serial.is_open:
                 line = self.serial.read_until(b"\r", 80)
                 self.parse_line(line.decode())
