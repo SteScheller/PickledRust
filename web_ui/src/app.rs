b8786539fff8607e6e5d06d03d0c36582ea40d97
Stefan Scheller <stefan.scheller@endress.com>
2023-06-14 15:47:13 +0200

    realized sending POST value via serial port

--- web_ui/src/app.rs
+++ web_ui/src/app.rs
@@ -5,6 +5,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+use regex::Regex;
 use serialport::SerialPort;
 
 pub struct Application {
@@ -16,30 +17,36 @@ impl Application {
     pub fn build(addr: &str, port: Box<dyn SerialPort>) -> Result<Application, Box<dyn Error>> {
         let listener = TcpListener::bind(addr)?;
 
-        Ok(Application { listener, port, })
+        Ok(Application { listener, port })
     }
 
-    pub fn run(self) {
+    pub fn run(mut self) {
         for stream in self.listener.incoming() {
             let stream = stream.unwrap();
-            self.handle_connection(stream);
+            Self::handle_connection(stream, &mut self.port);
         }
     }
 
-    fn handle_connection(&self, mut stream: TcpStream) {
+    fn handle_connection(mut stream: TcpStream, port: &mut Box<dyn SerialPort>) {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).expect("Failed to read tcp stream");
 
         let request = String::from_utf8_lossy(&buffer[..]);
-        println!("Request:\n{}", request);
+        let response = Self::execute_request(&request, port);
 
-        let request_line = &request.lines().collect::<Vec<_>>()[0];
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+
+    fn execute_request(request: &str, port: &mut Box<dyn SerialPort>) -> String {
+        let request_line = request.lines().collect::<Vec<_>>()[0];
         let (status_line, filename) = match &request_line[..] {
             "POST /set_led HTTP/1.1" => {
-                let key = "color=";
-                if let Some(value_start) = request.find(key) {
-                    let value = &request[value_start + key.len()..];
-                    println!("{value}");
+                let re_value = Regex::new(r"color=(\w+)").unwrap();
+                if let Some(c) = re_value.captures(request){
+                    let value = c.get(1).unwrap().as_str();
+                    let command = format!("led {value}\r");
+                    port.write(command.as_bytes())
+                        .expect("Failed to send command via serial port");
                 }
                 ("HTTP/1.1 200 OK", None)
             }
@@ -59,6 +66,6 @@ impl Application {
             0 => format!("{status_line}"),
             _ => format!("{status_line}\r\nContent-length: {length}\r\n\r\n{content}"),
         };
-        stream.write_all(response.as_bytes()).unwrap();
+        response
     }
 }
--- web_ui/src/main.rs
+++ web_ui/src/main.rs
@@ -1,8 +1,6 @@
 use std::{
     env,
     process::exit,
-    //thread,
-    //time::Duration,
 };
 
 use serialport;
@@ -21,34 +19,9 @@ fn main() {
         }
     };
 
-    /*
-        TODO: this does not seem to work with virtual ports opened with socat
-
-        thread 'main' panicked at 'Failed to open serial port: Error { kind: Unknown, description: "Not a typewriter" }'
-    */
-
-    let mut port = serialport::new(port, 9600)
+    let port = serialport::new(port, 9600)
         .open()
         .expect("Failed to open serial port");
-
-    /*
-    port.write(b"led off\r").unwrap();
-    thread::sleep(Duration::from_secs(1));
-    port.write(b"led red\r").unwrap();
-    thread::sleep(Duration::from_secs(1));
-
-    port.write(b"led off\r").unwrap();
-    thread::sleep(Duration::from_secs(1));
-    port.write(b"led green\r").unwrap();
-    thread::sleep(Duration::from_secs(1));
-
-    port.write(b"led off\r").unwrap();
-    thread::sleep(Duration::from_secs(1));
-    port.write(b"led blue\r").unwrap();
-    thread::sleep(Duration::from_secs(1));
-
-    port.write(b"led off\r").unwrap();
-    */
     let app = Application::build("127.0.0.1:7878", port).expect("Failed to start application");
     app.run();
 
